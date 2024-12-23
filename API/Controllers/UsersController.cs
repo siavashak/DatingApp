b983f51ace7e52021fa249a0f04394becee422dd
siavashak <siavash.akrami@gmail.com>
2024-12-23 03:35:08 -0800

    End of Section 4

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,21 +1,23 @@
 using System;
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-namespace API.Controllers;
-[ApiController]
-[Route("api/[controller]")]    // api/users
-public class UsersController(DataContext context) : ControllerBase
+namespace API.Controller;
+
+public class UsersController(DataContext context) : BaseApiController
 {
-    [HttpGet]
+    [AllowAnonymous]
+    [HttpGet]   // api/users
     public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
     {
         var users = await context.Users.ToListAsync();
         return users;
     }
 
+    [Authorize]
     [HttpGet("{id}")]   // api/users/3
     public async Task<ActionResult<AppUser>> GetUser(int id)
     {
---
+++ API/Extensions/IdentityServiceExtension.cs
@@ -0,0 +1,31 @@
+using System.Net;
+using System.Text;
+using API.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+
+namespace API.Extensions;
+
+public static class IdentityServiceExtensions
+{
+    public static IServiceCollection AddIdentityServices(this IServiceCollection services, IConfiguration config)
+    {
+        // Add services to the container.
+        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+        .AddJwtBearer(options =>
+        {
+            var tokenKey = config["TokenKey"] ?? throw new Exception("TokenKey not found");
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenKey)),
+                ValidateIssuer = false,
+                ValidateAudience = false
+            };
+        });
+
+
+        return services;
+    }
+}
